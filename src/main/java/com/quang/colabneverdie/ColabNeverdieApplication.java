4d86877203e554cf069d66087547c9a7b96cee08
quang <haoquangms@gmail.com>
2021-02-12 20:57:26 +0700

    init project

---
+++ src/main/java/com/quang/colabneverdie/ColabNeverdieApplication.java
@@ -0,0 +1,13 @@
+package com.quang.colabneverdie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColabNeverdieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColabNeverdieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quang/colabneverdie/ColabNeverdieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quang.colabneverdie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColabNeverdieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
